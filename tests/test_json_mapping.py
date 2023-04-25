5e87a66e75519448efed8aab0e11fbcb1f6cb983
jecarr <jenine_c@outlook.com>
2023-04-25 12:54:08 +0100

    Added test_country_values()

--- tests/test_json_mapping.py
+++ tests/test_json_mapping.py
@@ -1,13 +1,29 @@
 import os
 import json
+import requests
 import unittest
+
+THREAD_COUNTRY_CONF = 'https://raw.githubusercontent.com/arachne-threat-intel/' \
+                      'thread/main/threadcomponents/conf/countries-iso2.json'
 
 
 class TestJSONMapping(unittest.TestCase):
     """A test case for the JSON mappings file."""
     JSON_MAPPING_FILENAME = 'cta_names_mappings.json'
+    loaded_json = None
 
-    def test_valid_json(self):
-        self.assertTrue(os.path.isfile(self.JSON_MAPPING_FILENAME), 'JSON mapping file not found.')
-        with open(self.JSON_MAPPING_FILENAME) as json_mapping_opened:
-            json.load(json_mapping_opened)
+    @classmethod
+    def setUpClass(cls):
+        cls.assertTrue(os.path.isfile(cls.JSON_MAPPING_FILENAME), 'JSON mapping file not found.')
+        with open(cls.JSON_MAPPING_FILENAME) as json_mapping_opened:
+            cls.loaded_json = json.load(json_mapping_opened)
+
+    def test_country_values(self):
+        """Tests whether country codes within the JSON mapping file are valid."""
+        countries = requests.get(THREAD_COUNTRY_CONF).json()
+        country_codes = [c['alpha-2'].upper() for c in countries]
+        for group, entry in self.loaded_json.items():
+            entry_countries = entry.get('countries', [])
+            for entry_country in entry_countries:
+                error_msg = "Country '%s' under '%s' is not a valid country code." % (entry_country, group)
+                self.assertTrue(entry_country.upper() in country_codes, msg=error_msg)
